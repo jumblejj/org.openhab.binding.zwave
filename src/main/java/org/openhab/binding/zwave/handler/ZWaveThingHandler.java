e9342e98e870e39b1c9e3ce613194030f7ee43e1
jumblejj <32015310+jumblejj@users.noreply.github.com>
2021-01-24 16:01:58 +1100

    Update ZWaveThingHandler.java
    If incoming event recieved with out endpoint info, permit processing against corresponding channel for endpoint 1

--- src/main/java/org/openhab/binding/zwave/handler/ZWaveThingHandler.java
+++ src/main/java/org/openhab/binding/zwave/handler/ZWaveThingHandler.java
@@ -1369,12 +1369,17 @@ public class ZWaveThingHandler extends ConfigStatusThingHandler implements ZWave
                 logger.trace("NODE {}: Checking channel={}, cmdClass={}, endpoint={}", nodeId, channel.getUID(),
                         channel.getCommandClass(), channel.getEndpoint());
 
-                if (channel.getEndpoint() != event.getEndpoint()) {
-                    continue;
-                }
-
                 // Is this command class associated with this channel?
                 if (!channel.getCommandClass().equals(commandClass)) {
+                    continue;
+                }
+                
+                // Is it for the correct endpoint?
+                if (event.getEndpoint() == 0 && channel.getEndpoint() == 1)
+                {
+                    logger.debug("NODE {}: No endPoint information recieved, processing command {} for endpoint {} channel {}", nodeId, channel.getCommandClass(), channel.getEndpoint(), channel.getUID());
+                }
+                else if (channel.getEndpoint() != event.getEndpoint()) {
                     continue;
                 }
 
